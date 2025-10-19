2947f09535a5ef207382218042a9834b87ea92e0
Andrew-232 <ahern232@ucsc.edu>
2025-10-19 23:04:30 +0000

    Step 4 Complete!
    Made changes so the automatic counter increases with requestAnimationFrame
    Also added console.logs to display it in the page.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 let counter: number = 0;
 
 document.body.innerHTML = `
-  <h1>Clicker Game</h1>
-  <p>Counter: <span id="counter">0</span></p>
-  <button id="increment">Manuel Clicker Button!</button>
+ <h1>Clicker Game</h1>
+ <p>Counter: <span id="counter">0</span></p>
+ <button id="increment">Manuel Clicker Button!</button>
 `;
 
 document.body.style.backgroundColor = "lightgrey";
@@ -17,8 +17,26 @@ button.addEventListener("click", () => {
   counterElement.textContent = counter.toString();
 });
 
-// Added set interval where the counter hould increase by 1 every second.
-setInterval(() => {
-  counter++;
-  counterElement.textContent = counter.toString();
-}, 1000);
+let currentFrame: number = 0;
+
+//Function for the animation loop
+function gameLoop(timestamp: number) {
+  if (currentFrame === 0) {
+    currentFrame = timestamp;
+  }
+
+  const deltaTime = (timestamp - currentFrame) / 1000;
+
+  currentFrame = timestamp;
+
+  const growthThisFrame = 1 * deltaTime;
+
+  counter += growthThisFrame;
+
+  counterElement.textContent = Math.floor(counter).toString();
+
+  requestAnimationFrame(gameLoop);
+  console.log("requestion animation frame function is hopefully working!");
+}
+
+requestAnimationFrame(gameLoop);
