d1a124361d1c4619f2b645a71b6d10ad56f213bf
Andrew-232 <ahern232@ucsc.edu>
2025-10-19 23:08:31 +0000

    Step 5 Complete!
    Added code to create a new button which is locked until the player has enough to but it.
    Once bought the upgrade increase the clicker amount by 1 clicker per second.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 let counter: number = 0;
+let growthRate: number = 0;
+const upgradeCost: number = 10;
 
 document.body.innerHTML = `
  <h1>Clicker Game</h1>
  <p>Counter: <span id="counter">0</span></p>
+ <p>Auto Clickers Bought: <span id="growthRateDisplay">0</span></p>
  <button id="increment">Manuel Clicker Button!</button>
+ <button id="purchaseUpgrade" disabled>Purchase Auto Clicker Upgrade (Cost: ${upgradeCost})</button>
 `;
 
 document.body.style.backgroundColor = "lightgrey";
@@ -11,10 +15,24 @@ document.body.style.backgroundColor = "lightgrey";
 // Add click handler
 const button = document.getElementById("increment")!;
 const counterElement = document.getElementById("counter")!;
+const purchaseUpgradeButton = document.getElementById(
+  "purchaseUpgrade",
+) as HTMLButtonElement;
+const growthRateDisplay = document.getElementById("growthRateDisplay")!;
 
 button.addEventListener("click", () => {
   counter++;
   counterElement.textContent = counter.toString();
+});
+
+//Logic for the upgrade
+purchaseUpgradeButton.addEventListener("click", () => {
+  if (counter >= upgradeCost) {
+    counter -= upgradeCost;
+    growthRate++;
+    growthRateDisplay.textContent = growthRate.toString();
+    console.log("Upgrade bought!");
+  }
 });
 
 let currentFrame: number = 0;
@@ -29,14 +47,13 @@ function gameLoop(timestamp: number) {
 
   currentFrame = timestamp;
 
-  const growthThisFrame = 1 * deltaTime;
-
-  counter += growthThisFrame;
+  counter += growthRate * deltaTime;
 
   counterElement.textContent = Math.floor(counter).toString();
 
+  purchaseUpgradeButton.disabled = counter < upgradeCost;
+
   requestAnimationFrame(gameLoop);
-  console.log("requestion animation frame function is hopefully working!");
 }
 
 requestAnimationFrame(gameLoop);
