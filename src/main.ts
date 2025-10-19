80da5e9441a22a54f37bd2399c45933395c9f773
Andrew-232 <ahern232@ucsc.edu>
2025-10-19 23:01:22 +0000

    Step 3 Complete!
    Added a setInterval to increase the counter automatically by 1 every second.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-// Simple counter for demonstration
 let counter: number = 0;
 
 document.body.innerHTML = `
-  <h1>CMPM 121 Project</h1>
+  <h1>Clicker Game</h1>
   <p>Counter: <span id="counter">0</span></p>
-  <button id="increment">Click Me!</button>
+  <button id="increment">Manuel Clicker Button!</button>
 `;
+
+document.body.style.backgroundColor = "lightgrey";
 
 // Add click handler
 const button = document.getElementById("increment")!;
@@ -15,3 +16,9 @@ button.addEventListener("click", () => {
   counter++;
   counterElement.textContent = counter.toString();
 });
+
+// Added set interval where the counter hould increase by 1 every second.
+setInterval(() => {
+  counter++;
+  counterElement.textContent = counter.toString();
+}, 1000);
